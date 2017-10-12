1655f07c0f3c4a79817638b53cad16f235f3078b
Sagar Shah <shahsagar1111@gmail.com>
2017-10-12 15:19:45 +0530

    Task24 to 31 - Done

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return when{
+            this.year != other.year -> this.year - other.year
+            this.month != other.month -> this.month - other.month
+            else -> this.dayOfMonth - other.dayOfMonth
+        }
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +18,40 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun TimeInterval.times(int : Int):RepreatedIntervals{
+    return RepreatedIntervals(this,int)
+}
+
+operator fun MyDate.plus(timeInterval: TimeInterval) : MyDate {
+    return this.addTimeIntervals(timeInterval, 1)
+}
+
+operator fun MyDate.plus(timeInterval: RepreatedIntervals) : MyDate {
+
+    var date = this
+    for(i in 1..timeInterval.times){
+        date += timeInterval.inter
+    }
+    return date
+}
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>, Iterator<MyDate> {
+    var startDate = start
+    override fun hasNext(): Boolean {
+        return startDate <= endInclusive
+    }
+
+    override fun next(): MyDate {
+        var temp = startDate
+        startDate = startDate.nextDay()
+       return temp
+    }
+
+    override operator fun contains(value: MyDate): Boolean{
+        return value >= start && value <= endInclusive
+    }
+}
+class RepreatedIntervals(interval : TimeInterval, n : Int){
+    var inter = interval
+    var times = n
+}
--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,7 +3,15 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable {
+    var count = 0
+    fun getNumberOfInvocations() = count
+
+    operator fun invoke(): Invokable{
+        count++
+        return this
+    }
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +22,8 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+//    todoTask31()
+    return invokable()()()().getNumberOfInvocations()
 }
+
+
